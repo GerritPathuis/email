16a4a736ac5a38a1742d6deb494fcc5bfab6d02a
GerritPathuis <g.pathuis@quicknet.nl>
2016-08-03 20:07:35 +0200

    Update works

---
+++ Send_email_1/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(148, 63)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(194, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = ".."
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(148, 111)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(194, 20)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = ".."
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(148, 163)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(194, 50)
+        Me.TextBox3.TabIndex = 2
+        Me.TextBox3.Text = "Message"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(53, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "To:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(53, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 18)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "From:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(53, 165)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 18)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Message:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(56, 274)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(217, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Send"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(447, 322)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Send_email_1/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System
+Imports System.Net
+Imports System.Net.Mail
+
+Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        send_mail()
+    End Sub
+
+    Private Sub send_mail()
+        Dim Smtp_Server As New SmtpClient
+        Dim e_mail As New MailMessage()
+
+        Smtp_Server.UseDefaultCredentials = False
+        Smtp_Server.Credentials = New Net.NetworkCredential("g.pathuis@zoho.com", "BMWRS1200")
+        Smtp_Server.Port = 587
+        Smtp_Server.EnableSsl = True
+        Smtp_Server.Host = "smtp.zoho.com"
+        e_mail = New MailMessage()
+        e_mail.From = New MailAddress("g.pathuis@zoho.com")
+        e_mail.To.Add("g.pathuis@quicknet.nl")
+        e_mail.Subject = "Testing"
+        e_mail.IsBodyHtml = False
+        e_mail.Body = "This is the body content send in VB.net test4"
+
+        Smtp_Server.Send(e_mail)
+    End Sub
+End Class
+
+
+
+
